0a8b23a897f05dd595a74baad23c5d22b2bfa29e
Jeffrey Paulouse <jeffreypaulousem24@gmail.com>
2025-06-12 13:43:13 +0530

    practice 6

--- index.js
+++ index.js
@@ -374,8 +374,6 @@ else{
 let result2 = userName.endsWith(" ");
 console.log(result2)
 
-*/
-
 let phoneNumber = "701-939-2700";
 phoneNumber = phoneNumber.replaceAll("-","/");
 console.log(phoneNumber);
@@ -385,3 +383,172 @@ console.log(phoneNumber);
 
 phoneNumber = phoneNumber.padEnd(25,"0");
 console.log(phoneNumber);
+
+//string slicing
+
+const fullName = "Jeffrey Paulouse";
+
+let firstName = fullName.slice(0,7);
+console.log(firstName);
+let lastName = fullName.slice(8,16);
+console.log(lastName);
+let firstChar = fullName.slice(0,1);
+console.log(firstChar);
+let lastChar = fullName.slice(-1);
+console.log(lastChar);
+
+const fullName = "Jeffrey Paulouse";
+
+let firstName = fullName.slice(0, fullName.indexOf(" "));
+console.log(firstName);
+let lastName = fullName.slice(fullName.indexOf(" ") + 1);
+console.log(lastName);
+
+const email = "jeffreyapulousem24@gmail.com";
+
+let username = email.slice(0,email.indexOf("@"));
+console.log(username);
+let extension = email.slice(email.indexOf("@") + 1);
+console.log(extension);
+
+//no method training
+
+let username = window.prompt("Enter your username");
+username = username.trim();
+let letter = username.charAt(0);
+letter = letter.toUpperCase();
+let extraChars = username.slice(1);
+extraChars = extraChars.toLowerCase();
+username = letter + extraChars;
+console.log(username);      
+
+//Method chaining
+
+let username = window.prompt("Enter your username");
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+console.log(username);              
+
+//Logical Operators
+
+// &&  AND 
+
+const temp = 21;
+
+if(temp > 0 && temp <=30) {
+    console.log("Weather is good");
+}
+else {
+    console.log("The weather is bad");
+}
+
+// || OR
+
+const temp = 25;
+if(temp <= 0 || temp > 30) {
+    console.log("Weather is BAD");
+}
+else {
+    console.log("The weather is GOOD");
+} 
+
+// ! NOT
+
+const isSunny = true;
+ if(!isSunny) {
+    console.log("It is cloudy");
+ }
+ else{
+    console.log("It is sunny");
+ }
+
+//Strict equality operator (===) compares values and datatypes
+
+const PI = 3.14;
+if(PI === "3.14"){
+    console.log("That is PI");
+}
+else{
+    console.log("That is not PI");
+}
+
+//inequality operator (!=)
+
+const PI = 3.14;
+if(PI != "3.14"){
+    console.log("That is not PI");
+}
+else{
+    console.log("That is PI");
+}
+
+//strict inequality operator (!==)
+
+const PI = 3.14;
+if(PI !== "3.14"){
+    console.log("That is not PI");
+}
+else{
+    console.log("That is PI");
+}
+
+//While loops:loops as long as condition is true
+
+let username = "";
+
+while(username === "" || username === null){
+    username = window.prompt("Enter your name: ");
+}
+    console.log(`Hello ${username}`);
+
+//do while
+
+let username;
+do{
+    username = window.prompt("Enter your name: ");
+}
+    while(username === "" || username === null)
+    console.log(`Hello ${username}`);
+ 
+//example 2
+
+let loggedIn = false;
+let username;
+let password;
+while(!loggedIn){
+    username = window.prompt("Enter your username: ");
+    password = window.prompt("Enter your password: ");
+
+    if(username === "myUSername" && password === "myPassword"){
+        loggedIn = true;
+        console.log("You are logged in");
+    }
+}
+
+//for loops : repeat code a specific number of times
+
+for(let i = 10; i > 0; i-=1){
+    console.log(i);
+}
+console.log("Happy birthday");
+
+//continue
+for(let i = 1; i <=20; i++){
+    if(i = 13){
+        continue;
+    }
+    else{
+        console.log(i);
+    }
+}
+
+//break
+for(let i = 1; i <=20; i++){
+    if(i = 13){
+        break;
+    }
+    else{
+        console.log(i);
+    }
+}
+       
+*/
